51046efb503fdd7ef146723c55d49f24f0f5c5c2
annisaseptyana <starrynyteskye@gmail.com>
2022-07-19 02:07:55 +0700

    Implementing random habit on ActionBar menu

--- app/src/main/java/com/dicoding/habitapp/ui/list/HabitListActivity.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/list/HabitListActivity.kt
@@ -13,9 +13,11 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.habitapp.R
 import com.dicoding.habitapp.data.Habit
+import com.dicoding.habitapp.setting.SettingsActivity
 import com.dicoding.habitapp.ui.ViewModelFactory
 import com.dicoding.habitapp.ui.add.AddHabitActivity
 import com.dicoding.habitapp.ui.detail.DetailHabitActivity
+import com.dicoding.habitapp.ui.random.RandomHabitActivity
 import com.dicoding.habitapp.utils.Event
 import com.dicoding.habitapp.utils.HABIT_ID
 import com.dicoding.habitapp.utils.HabitSortType
@@ -75,11 +77,22 @@ class HabitListActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return true
+        return when(item.itemId) {
+            R.id.action_random -> {
+                startActivity(Intent(this@HabitListActivity, RandomHabitActivity::class.java))
+                true
+            }
+            R.id.action_settings -> {
+                startActivity(Intent(this@HabitListActivity, SettingsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun showFilteringPopUpMenu() {
--- app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
@@ -3,15 +3,17 @@ package com.dicoding.habitapp.ui.random
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.habitapp.R
 import com.dicoding.habitapp.data.Habit
 
-class RandomHabitAdapter(
-    private val onClick: (Habit) -> Unit
-) : RecyclerView.Adapter<RandomHabitAdapter.PagerViewHolder>() {
+class RandomHabitAdapter(private val onClick: (Habit) -> Unit) : RecyclerView.Adapter<RandomHabitAdapter.PagerViewHolder>() {
 
     private val habitMap = LinkedHashMap<PageType, Habit>()
+
 
     fun submitData(key: PageType, habit: Habit) {
         habitMap[key] = habit
@@ -39,9 +41,24 @@ class RandomHabitAdapter(
     inner class PagerViewHolder internal constructor(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
         //TODO 14 : Create view and bind data to item view
+        private val tvTitle: TextView = itemView.findViewById(R.id.pager_tv_title)
+        private val tvStartTime: TextView = itemView.findViewById(R.id.pager_tv_start_time)
+        private val tvMinutes: TextView = itemView.findViewById(R.id.pager_tv_minutes)
+        private val itemPriorityLevel: ImageView = itemView.findViewById(R.id.item_priority_level)
+        private val btnOpenCountDown: Button = itemView.findViewById(R.id.btn_open_count_down)
 
         fun bind(pageType: PageType, pageData: Habit) {
+            tvTitle.text = pageData.title
+            tvStartTime.text = pageData.startTime
+            tvMinutes.text = pageData.minutesFocus.toString()
 
+            when(pageType) {
+                PageType.HIGH -> itemPriorityLevel.setImageResource(R.drawable.ic_priority_high)
+                PageType.MEDIUM -> itemPriorityLevel.setImageResource(R.drawable.ic_priority_medium)
+                PageType.LOW -> itemPriorityLevel.setImageResource(R.drawable.ic_priority_low)
+            }
+
+            btnOpenCountDown.setOnClickListener { onClick(pageData) }
         }
     }
 }
