6fbea4040d7d4e24d8b78a5379f5a2927ca11b16
annisaseptyana <starrynyteskye@gmail.com>
2022-07-19 00:00:48 +0700

    Adding Notification

--- app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
@@ -1,13 +1,23 @@
 package com.dicoding.habitapp.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.detail.DetailHabitActivity
 import com.dicoding.habitapp.utils.HABIT_ID
 import com.dicoding.habitapp.utils.HABIT_TITLE
+import com.dicoding.habitapp.utils.NOTIFICATION_CHANNEL_ID
 
-class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+class NotificationWorker(private val ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
     private val habitId = inputData.getInt(HABIT_ID, 0)
     private val habitTitle = inputData.getString(HABIT_TITLE)
@@ -17,8 +27,42 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
         val shouldNotify = prefManager.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
 
         //TODO 12 : If notification preference on, show notification with pending intent
+        if (shouldNotify) {
+            val intent = Intent(ctx, DetailHabitActivity::class.java)
+            intent.putExtra(HABIT_ID, habitId)
+            val pendingIntent = PendingIntent.getActivity(ctx, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val message = ctx.getString(R.string.notify_content)
+            val title = habitTitle
+            val mNotificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val mNotificationManager = ctx.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val mBuilder = NotificationCompat.Builder(ctx, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notifications)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(ctx, android.R.color.transparent))
+                .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+                .setSound(mNotificationSound)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
 
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val mChannel = NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID,
+                    "notify-habit",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+
+                mChannel.enableVibration(true)
+                mChannel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
+                mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID)
+                mNotificationManager.createNotificationChannel(mChannel)
+            }
+            mBuilder.setAutoCancel(true)
+            val mNotification = mBuilder.build()
+            mNotificationManager.notify(1, mNotification)
+        }
         return Result.success()
     }
-
 }
